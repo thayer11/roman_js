d388f9b67fa18cb882bcee1c43f426a6aa5ec316
RobertAKARobin <robertgfthomas@gmail.com>
2015-06-16 18:53:26 -0400

    sol

--- src/Roman.js
+++ src/Roman.js
@@ -1,15 +1,28 @@
-function romanize(number){
-  var matches = {
-    1: "I",
-    2: "II",
-    3: "III",
-    4: "IV",
-    5: "V",
-    6: "VI",
-    7: "VII",
-    8: "VIII",
-    9: "IX",
-    10: "X"
+var romanize = function (number) {
+  result = '';
+  mappings = [
+    {arabic: 1000, roman: 'M'},
+    {arabic: 900, roman: 'CM'},
+    {arabic: 500, roman: 'D'},
+    {arabic: 400, roman: 'CD'},
+    {arabic: 100, roman: 'C'},
+    {arabic: 90, roman: 'XC'},
+    {arabic: 50, roman: 'L'},
+    {arabic: 40, roman: 'XL'},
+    {arabic: 10, roman: 'X'},
+    {arabic: 9, roman: 'IX'},
+    {arabic: 5, roman: 'V'},
+    {arabic: 4, roman: 'IV'},
+    {arabic: 1, roman: 'I'}
+  ];
+
+  for (var i = 0; i < mappings.length; i++) {
+    var mapping = mappings[i];
+    while (number >= mapping.arabic) {
+      result += mapping.roman;
+      number -= mapping.arabic;
+    }
   }
-  return matches[number];
-}
+  
+  return result;
+};
